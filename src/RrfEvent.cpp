1fb3b7039abeaeadb6b339439ed2723b6e62d40d
Luchinsky <alexey.luchinsky@gmail.com>
2019-11-15 06:41:32 -0500

    Empty event class is added

---
+++ src/RrfEvent.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   RrfEvent.cpp
+ * Author: luchinsky
+ * 
+ * Created on November 15, 2019, 6:38 AM
+ */
+
+#include "RrfEvent.h"
+
+RrfEvent::RrfEvent() {
+}
+
+RrfEvent::RrfEvent(const RrfEvent& orig) {
+}
+
+RrfEvent::~RrfEvent() {
+}
+
---
+++ src/RrfEvent.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   RrfEvent.h
+ * Author: luchinsky
+ *
+ * Created on November 15, 2019, 6:38 AM
+ */
+
+#ifndef RRFEVENT_H
+#define RRFEVENT_H
+
+class RrfEvent {
+public:
+    RrfEvent();
+    RrfEvent(const RrfEvent& orig);
+    virtual ~RrfEvent();
+private:
+
+};
+
+#endif /* RRFEVENT_H */
+
