724ce30805004632fc4ebbd3a75532ec6ab677d5
Luchinsky <alexey.luchinsky@gmail.com>
2019-11-16 06:50:06 -0500

    Empty var class added

---
+++ src/RrfVar.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   RrfVar.cpp
+ * Author: luchinsky
+ * 
+ * Created on November 16, 2019, 6:48 AM
+ */
+
+#include "RrfVar.h"
+
+RrfVar::RrfVar() {
+}
+
+RrfVar::RrfVar(const RrfVar& orig) {
+}
+
+RrfVar::~RrfVar() {
+}
+
---
+++ src/RrfVar.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   RrfVar.h
+ * Author: luchinsky
+ *
+ * Created on November 16, 2019, 6:48 AM
+ */
+
+#ifndef RRFVAR_H
+#define RRFVAR_H
+
+class RrfVar {
+public:
+    RrfVar();
+    RrfVar(const RrfVar& orig);
+    virtual ~RrfVar();
+private:
+
+};
+
+#endif /* RRFVAR_H */
+
