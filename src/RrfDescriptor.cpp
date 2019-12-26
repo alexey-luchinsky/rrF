279c6cd004af5a7b9880caf287e304ac4edb2840
Luchinsky <alexey.luchinsky@gmail.com>
2019-12-26 11:07:20 -0500

    Empty source file for decay descriptor is added

---
+++ src/RrfDescriptor.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   RrfDescriptor.cpp
+ * Author: luchinsky
+ * 
+ * Created on December 26, 2019, 10:52 AM
+ */
+
+#include "RrfDescriptor.h"
+
+RrfDescriptor::RrfDescriptor() {
+}
+
+RrfDescriptor::RrfDescriptor(const RrfDescriptor& orig) {
+}
+
+RrfDescriptor::~RrfDescriptor() {
+}
+
---
+++ src/RrfDescriptor.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   RrfDescriptor.h
+ * Author: luchinsky
+ *
+ * Created on December 26, 2019, 10:52 AM
+ */
+
+#ifndef RRFDESCRIPTOR_H
+#define RRFDESCRIPTOR_H
+
+class RrfDescriptor {
+public:
+    RrfDescriptor();
+    RrfDescriptor(const RrfDescriptor& orig);
+    virtual ~RrfDescriptor();
+private:
+
+};
+
+#endif /* RRFDESCRIPTOR_H */
+
