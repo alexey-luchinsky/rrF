e64755674c7c76270a3052aced48ae5e7fd5c6fc
Luchinsky <alexey.luchinsky@gmail.com>
2020-01-18 11:15:39 -0500

    empty test file is added

---
+++ src/segR_test.cc
@@ -0,0 +1,19 @@
+/* 
+ * File:   segR_test.cc
+ * Author: luchinsky
+ *
+ * Created on January 18, 2020, 11:12 AM
+ */
+
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout << "qq" << endl;
+    return 0;
+}
+
