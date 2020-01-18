c86a36ae0c47d99d188e65875d37bc87b9d348c7
Luchinsky <alexey.luchinsky@gmail.com>
2020-01-18 11:59:41 -0500

    Empty probe EvtGen model is created

---
+++ src/EvtGenModels/EvtProbeModel.hh
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   EvtProbeModel.hh
+ * Author: luchinsky
+ *
+ * Created on January 18, 2020, 11:57 AM
+ */
+
+#ifndef EVTPROBEMODEL_HH
+#define EVTPROBEMODEL_HH
+
+class EvtProbeModel {
+public:
+    EvtProbeModel();
+    EvtProbeModel(const EvtProbeModel& orig);
+    virtual ~EvtProbeModel();
+private:
+
+};
+
+#endif /* EVTPROBEMODEL_HH */
+
---
+++ src/EvtProbeModel.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   EvtProbeModel.cpp
+ * Author: luchinsky
+ * 
+ * Created on January 18, 2020, 11:57 AM
+ */
+
+#include "EvtGenModels/EvtProbeModel.hh"
+
+EvtProbeModel::EvtProbeModel() {
+}
+
+EvtProbeModel::EvtProbeModel(const EvtProbeModel& orig) {
+}
+
+EvtProbeModel::~EvtProbeModel() {
+}
+
